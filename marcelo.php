4e02a0d04373b531d3004096b8b5e25fa7a9c907
Ryan Lin <linfuz88@gmail.com>
2018-09-17 16:45:13 -0400

    conglomerated all pages

---
+++ marcelo.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Lab 01</title>
+
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="node_modules/normalize.css/normalize.css">
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+  <div class="hero">
+    <nav>
+      <div class="header">
+        <p>Steve Jobs</p>
+      </div>
+
+      <div class="navigation">
+        <a href="index.html" class="active">Home</a>
+        <a href="about.html">About</a>
+        <a href="innovations.html">Innovations</a>
+        <a href="media.html">Media</a>
+      </div>
+    </nav>
+    <div class="hero-text stroke">
+      <h1>Steve Jobs</h1>
+      <p><i>Innovation • Technology • Apple</i></p>
+      <p>Steven Paul Jobs (/dʒɒbz/; February 24, 1955 – October 5, 2011) was an American entrepreneur and business magnate. Jobs is widely recognized as a pioneer of the microcomputer revolution of the 1970s and 1980s, along with Apple co-founder Steve Wozniak. </p>
+      <p>Jobs was born in San Francisco, California, to parents who put him up for adoption at birth. He was raised in the San Francisco Bay Area during the 1960s. He attended Reed College in 1972 before dropping out that same year, and traveled through India in 1974 seeking enlightenment and studying Zen Buddhism. His declassified FBI report states that he used marijuana and LSD while he was in college, and once told a reporter that taking LSD was "one of the two or three most important things" he had done in his life.</p>
+      <p>Apple II - Apple Lisa - Macintosh - NeXT Computer - iMac - iPod - iPhone - iPad</p>
+      <h4>Feature films</h4>
+      <ul>
+        <li><p>2015: Steve Jobs: a feature film directed by Danny Boyle, with a screenplay by Aaron Sorkin. Jobs is portrayed by Michael Fassbender.</p></li>
+        <li><p>2013: Jobs: an independent film directed by Joshua Michael Stern. Jobs is portrayed by Ashton Kutcher.</p></li>
+        <li><p>2013: iSteve: a satirical film Directed by: Ryan Perez in which Jobs is portrayed by Justin Long.</p></li>
+        <li><p>1999: Pirates of Silicon Valley: a TNT film directed by Martyn Burke. Jobs is portrayed by Noah Wyle.</p></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>

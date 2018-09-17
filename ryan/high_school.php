a643ffbd84de861a3dcd106b61eac13f2da453b9
Ryan Lin <linfuz88@gmail.com>
2018-09-17 16:03:01 -0400

    copied files from server
    made new folders for each person

---
+++ ryan/high_school.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/navigation.css">
+  <meta charset="utf-8">
+  <title>Ryan Lin</title>
+</head>
+<body>
+  <header>
+    Ryan Lin's Life
+    <?php include("menu.php"); ?>
+  </header>
+  <article>
+    In high school, I gravitated toward math and science classes, taking the most advanced courses I could. I finished BC Calculus by junior year, and so took a linear algebra class in high school. I was similarly ambitious in the other subjects, taking all the AP’s I could, but wasn’t nearly as successful. History was a constant struggle to keep on top of dozens of dates and events which seemed so, so unimportant. English was also tough, as I could also not bring myself to see why studying Shakespeare would ever be relevant, but overall it was much more manageable. I wasn’t completely uncultured though, as my love for music kept me in school choirs, after school theater, and student run acapella organizations. Outside of school, I practiced piano and took voice lessons.
+  </article>
+    <aside>
+      <img src="images/IMG_3481.JPG" alt="me" height="240" width="160">
+    </aside>
+    <footer>
+      <p>Ryan Lin CSC 174 Webpage Design and Development</p>
+    </footer>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+  </html>
---
+++ ryan/menu.php
@@ -0,0 +1,12 @@
+<button class="hamburger">&#9776;</button>
+<button class="cross">&#735;</button>
+<!-- code for hamburger menu taken (and improved upon) from http://codepen.io/g13nn/pen/eHGEF -->
+
+<nav class="menu">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="high_school.php">High School</a></li>
+    <li><a href="work.php">Work</a></li>
+    <li><a href="college.php">College</a></li>
+  </ul>
+</nav>
